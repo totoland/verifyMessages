f9966cf5e88335f7768753bb771883e3003e2a04
Thanapong Niamnim <Thanapong.n@2c2p.com>
2021-01-11 21:36:17 +0700

    Add new feature B

--- src/main/java/com/toto/keytool/App.java
+++ src/main/java/com/toto/keytool/App.java
@@ -21,22 +21,25 @@ public class App {
 
         //Encrypt the message
         String cipherText = SecureMessage.encrypt(message, pair.getPublic());
-        
-        System.out.println("encrypt sender message : "+ cipherText);
+
+        System.out.println("encrypt sender message : " + cipherText);
 
         //Now decrypt it
         String decipheredMessage = SecureMessage.decrypt(cipherText, pair.getPrivate());
-        
-        System.out.println("decrypt message : "+ decipheredMessage);
+
+        System.out.println("decrypt message : " + decipheredMessage);
 
         //sign our message
         String signature = SecureMessage.sign("toto", pair.getPrivate());
 
-        System.out.println("signature : "+ signature);
-        
+        System.out.println("signature : " + signature);
+
         //check the signature
         boolean isCorrect = SecureMessage.verify("toto", signature, pair.getPublic());
         System.out.println("Signature correct: " + isCorrect);
     }
 
+    public void featureB() {
+        System.out.println("This is B.");
+    }
 }
